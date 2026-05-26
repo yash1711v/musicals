54f650ef5e91a92518374f1a2b8250f7df9b2dd5
Yash Verma <yv48183@gmail.com>
2026-05-26 17:18:13 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/yash/music_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.yash.music_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/domain/entities/audio_track_enty.dart
@@ -0,0 +1,7 @@
+class AudioTrackEntity {
+  late final String id;
+  late final String title;
+  late final int bpm;
+  late final Duration duration;
+  late final String encryptedPath;
+}
